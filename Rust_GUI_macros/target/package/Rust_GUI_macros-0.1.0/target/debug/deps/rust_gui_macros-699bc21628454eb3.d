f612ea77a6513f1c850eded328f68f38f6057b5f
AdrianD873 <a_dosch@gmx.de>
2022-04-25 22:31:22 +0200

    changes to publish to crates.io

--- Rust_GUI_macros/target/package/Rust_GUI_macros-0.1.0/target/debug/deps/rust_gui_macros-699bc21628454eb3.d
+++
@@ -1,7 +0,0 @@
-C:\dev\rust\Rust-GUI\Rust_GUI_macros\target\debug\deps\rust_gui_macros-699bc21628454eb3.rmeta: src\lib.rs
-
-C:\dev\rust\Rust-GUI\Rust_GUI_macros\target\debug\deps\rust_gui_macros-699bc21628454eb3.d: src\lib.rs
-
-src\lib.rs:
-
-# env-dep:CLIPPY_ARGS=
